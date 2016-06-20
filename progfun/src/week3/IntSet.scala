8bbb101c4eee81ed029f757de4d8a8a4f68f9b1a
Lodewijk Pool <lodewijk.pool@here.com>
2016-06-21 00:05:53 +0200

    Week 3 worksheets

--- progfun/src/week3/IntSet.scala
+++ progfun/src/week3/IntSet.scala
@@ -1,6 +1,42 @@
 package week3
 
 abstract class IntSet {
+
   def incl(x: Int): IntSet
   def contains(x:Int): Boolean
+  def union(other: IntSet): IntSet
+}
+
+object Empty extends IntSet {
+
+  override def contains(x: Int): Boolean = false
+
+  override def incl(x: Int): IntSet = new NonEmpty(x, Empty, Empty)
+
+  override def union(other: IntSet): IntSet = {
+    other
+  }
+
+  override def toString = "."
+}
+
+class NonEmpty(elem: Int, left: IntSet, right: IntSet) extends IntSet {
+
+  override def incl(x: Int): IntSet = {
+    if (x < elem) new NonEmpty(elem, left incl x, right)
+    else if (x > elem) new NonEmpty(elem, left, right incl x)
+    else this
+  }
+
+  override def contains(x: Int): Boolean = {
+    if (x < elem) left contains x
+    else if (x > elem) right contains x
+    else true
+  }
+
+  override def union(other: IntSet): IntSet = {
+    ((left union right) union other) incl elem
+  }
+
+  override def toString = "{" + left + elem + right + "}"
 }
---
+++ progfun/src/week3/IntSets.sc
@@ -0,0 +1,7 @@
+import week3._
+
+val t1 = new NonEmpty(3, new Empty, new Empty)
+val t2 = t1 incl 4
+
+
+
